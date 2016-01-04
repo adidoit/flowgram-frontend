0f0eb5b2e3e7782c57ee3b00cc4eff6aec041e67
adi pradhan <adip@umich.edu>
2016-01-04 22:07:07 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 
 var s = Snap("#svg");
 
-Snap.load("svg/facebook.svg",onSVGLoaded);
-
-Snap.load("svg/loadbalancer.svg",onSVGLoaded);
-
-function onSVGLoaded(data){
-	s.append(data);
-}
+$('#btn-success').on('click',function(){
+	swal({   title: 'Approved',   text: 'The flowgram has been approved',   type: 'success',   confirmButtonText: 'Close' });
+	$('#btn-success').attr('disabled','disabled');
+	$('#btn-danger').removeAttr('disabled');
+})
 
 
-swal({   title: 'Error!',   text: 'Do you want to continue',   type: 'error',   confirmButtonText: 'Cool' });
+$('#btn-danger').on('click',function(){
+	swal({   title: 'Rejected',   text: 'The flowgram has been rejected',   type: 'error',   confirmButtonText: 'Close' });
+	$('#btn-danger').attr('disabled','disabled');
+	$('#btn-success').removeAttr('disabled');
+})
+
+
+
+
+$("#btn-warning").on('click',function(){
+	$('#btn-success').removeAttr('disabled');
+	$('#btn-danger').removeAttr('disabled');
+})
