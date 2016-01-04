3798866260fb9bacf4e1862e96329e8a35b006e4
adi pradhan <adip@umich.edu>
2016-01-04 23:23:12 +0400

    freaking dependency order argh

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 
-var s = Snap("#svg");
-
-$('#btn-success').on('click',function(){
-	swal({   title: 'Approved',   text: 'The flowgram has been approved',   type: 'success',   confirmButtonText: 'Close' });
-	$('#btn-success').attr('disabled','disabled');
-	$('#btn-danger').removeAttr('disabled');
-})
-
-
-$('#btn-danger').on('click',function(){
-	swal({   title: 'Rejected',   text: 'The flowgram has been rejected',   type: 'error',   confirmButtonText: 'Close' });
-	$('#btn-danger').attr('disabled','disabled');
-	$('#btn-success').removeAttr('disabled');
-})
+$( document ).ready(function() {
+	console.log('in main.js');
+	$('#btn-success').on('click',function(){
+		swal({   title: 'Approved',   text: 'The flowgram has been approved',   type: 'success',   confirmButtonText: 'Close' });
+		$('#btn-success').attr('disabled','disabled');
+		$('#btn-danger').removeAttr('disabled');
+	})
 
 
+	$('#btn-danger').on('click',function(){
+		swal({   title: 'Rejected',   text: 'The flowgram has been rejected',   type: 'error',   confirmButtonText: 'Close' });
+		$('#btn-danger').attr('disabled','disabled');
+		$('#btn-success').removeAttr('disabled');
+	})
 
 
-$("#btn-warning").on('click',function(){
-	$('#btn-success').removeAttr('disabled');
-	$('#btn-danger').removeAttr('disabled');
-})
+
+
+	$("#btn-warning").on('click',function(){
+		$('#btn-success').removeAttr('disabled');
+		$('#btn-danger').removeAttr('disabled');
+	})
+
+});
