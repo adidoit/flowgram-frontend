ed6aa7b2121bad71c91800a3f4e074b216fd2362
adi pradhan <adip@umich.edu>
2016-01-05 15:22:38 +0400

    taggle working.. added fontawesome

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
 $( document ).ready(function() {
 	console.log('in main.js');
+	$(function () {
+  		$('[data-toggle="tooltip"]').tooltip()
+	})
+
+	$(function () {
+  		$('[data-toggle="popover"]').popover()
+	})
+
 	$('#btn-success').on('click',function(){
 		swal({   title: 'Approved',   text: 'The flowgram has been approved',   type: 'success',   confirmButtonText: 'Close' });
 		$('#btn-success').attr('disabled','disabled');
@@ -14,12 +22,14 @@ $( document ).ready(function() {
 		$('#btn-success').removeAttr('disabled');
 	})
 
-
-
-
 	$("#btn-warning").on('click',function(){
 		$('#btn-success').removeAttr('disabled');
 		$('#btn-danger').removeAttr('disabled');
 	})
-
+     
+    new Taggle('taggle', {
+    tags: ['web', 'e-commerce', 'database', 'paypal','facebook']
 });
+});
+
+
